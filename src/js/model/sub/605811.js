6a92f71056ac3219181c75355cec5de4ebcfc37b
5004 <5004>
2017-07-14 18:01:43 +0900

    update sub modularity

---
+++ src/js/model/sub/605811.js
@@ -0,0 +1,22 @@
+module.exports = {
+    imgs: {
+        i1: './img/fan1.jpg',
+        i2: './img/fan2.jpg',
+        i3: './img/fan3.jpg'
+    },
+    id: '605811',
+    name: '17년형 리모컨 Aero9 프리미엄 써큘레이터',
+    price: '73,000',
+    orderDays: '2017.7.10 오후 05시 ~ 2017.7.25 오후 05시',
+    orderSchedule: [ // [대괄호 안하면 인식 안함
+        {schedule: '- 17.7.10 ~ 17.7.12 까지 주문 고객 : 7월 14일 배송 예정'},
+        {schedule: '- 17.7.13 ~ 17.7.16 까지 주문 고객 : 7월 18일 배송 예정'},
+        {schedule: '- 17.7.17 ~ 17.7.19 까지 주문 고객 : 7월 21일 배송 예정'},
+        {schedule: '- 17.7.20 ~ 17.7.25 까지 주문 고객 : 7월 26일 배송 예정'}
+    ], 
+    timezone: 'Pacific/Guam',
+    location: {
+        lat: 13.444304,
+        lng: 144.793732
+    }
+};
--- src/js/sub.js
+++ src/js/sub.js
@@ -2,6 +2,33 @@ require('bootstrap');
 require('../less/sub.less');
 require('./sub-footer');
 
+// url-serch-params 쓰고 싶으면 가져 와야한다.
+var URLSearchParams = require('url-search-params');
+var params = new URLSearchParams(location.search);
+var theresId = params.get('id');
+
+try {
+    var model = require('./model/sub/' + theresId);
+}
+catch (e) {
+    var model = require('./model/sub/605811');
+}
+var carouselImgs = $('.carousel-inner').find('img');
+// 태그 src에는 url() 안들어간다. - 지움
+$(carouselImgs[0]).attr('src', model.imgs.i1);
+$(carouselImgs[1]).attr('src', model.imgs.i2);
+$(carouselImgs[2]).attr('src', model.imgs.i3);
+$('.sub-detail-title').html(model.name);
+$('.sub-detail-price').html(model.price + '원');
+$('.sub-box-info-day').html('주문기간 :' + model.orderDays);
+
+// handlebars 사용한 list 추가하기 template 필요
+// 모델 삽입은 template(model) - html이 만들어진 것
+// 태그에 삽입
+var schedulesTemplate = require('../template/sub/schedule.hbs');
+var schedulesHtml = schedulesTemplate(model);
+$('.sub-box-info-items').html('배송예정일' + schedulesHtml);
+
 $('.header-title').on('click', function() {
     location.href = 'index.html';
 });
@@ -27,3 +54,31 @@ $('.sub-select > li').on('click', function () {
     // id 찾을 때는 속성이니까 attr
 
 });
+// list의 item 가져오려면 ul > li
+$('.sub-faq-contents > li').on('click', function () {
+    if ($(this).find('div').hasClass('active')) {
+        $(this).css('background-color', '#ffffff');
+        $(this).find('div').removeClass('active');
+        return;
+    }
+    $(this).css('background-color', '#f7f7f7');
+    $(this).find('div').addClass('active');
+});
+
+$('.sub-items-info > li').on('click', function () {
+    var divItems = $(this).find('div');
+    var tagI = $(divItems[0]).find('i');
+    if ($(divItems[1]).hasClass('active')) {
+        $(divItems[0]).css('background-color', '#ffffff');
+        tagI.removeClass('fa-chevron-up');
+        tagI.addClass('fa-chevron-down');
+        $(divItems[1]).removeClass('active');
+        return;
+    }
+    $(divItems[0]).css('background-color', '#f0f0f0');
+    tagI.removeClass('fa-chevron-down');
+    tagI.addClass('fa-chevron-up');
+    $(divItems[1]).addClass('active');
+});
+
+
