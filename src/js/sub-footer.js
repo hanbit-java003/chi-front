3e2ab56262ed1e94b652876943dea0d75333fb61
5004 <5004>
2017-07-27 17:18:47 +0900

    sub-order-layer 추가

--- src/js/sub-footer.js
+++ src/js/sub-footer.js
@@ -1,1 +1,34 @@
 require('../less/sub-footer.less');
+
+$('.sub-footer-order').on('click', function() {
+    $('body').append('<div class="overlay-layer dark-layer"></div>');
+    $('body').css('overflow', 'hidden');
+
+    var memberLayer = require('../template/sub-footer-order-layer.hbs');
+
+    $('.sub-footer').append(memberLayer);
+
+    $('.sub-order-layer').animate({
+        bottom: '0px'
+    }, {
+        duration: 500,
+        complete: function() {
+            $('.overlay-layer').on('click', cancel);
+        }
+    });
+
+    $('.sub-order-btn-cancel').on('click', cancel);
+
+    function cancel() {
+        $('.sub-order-layer').animate({
+            bottom: '-161px'
+        }, {
+            duration: 500,
+            complete: function () {
+                $('.sub-order-layer').remove();
+                $('.overlay-layer').remove();
+                $('body').css('overflow', 'auto');
+            }
+        });
+    }
+});
