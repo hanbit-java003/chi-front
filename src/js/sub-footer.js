72766dc9899d7e9f6b0d0c0182e903fb028787f6
5004 <5004>
2017-08-17 16:26:43 +0900

    makers sub-footer order option UI

--- src/js/sub-footer.js
+++ src/js/sub-footer.js
@@ -1,24 +1,18 @@
 require('../less/sub-footer.less');
 
 var options = [{
-        option: '아쿠아블루레몬'
-    }, {
-        option: '블루체리머스크'
-    }, {
-        option: '일랑일랑아이리스'
-    }, {
-        option: '스윗피치자몽'
+    option: '아쿠아블루레몬',
+    price: 32000
+}, {
+    option: '블루체리머스크',
+    price: 32000
+}, {
+    option: '일랑일랑아이리스',
+    price: 32000
+}, {
+    option: '스윗피치자몽',
+    price: 32000
 }];
-
-function init() {
-    for (var i=0; i<options.length; i++) {
-        var html = '<li>' + options[i].option + '</li>';
-        $('.sub-order-option-list').append(html);
-        여기서부터 append가 왜 않붙지?
-    }
-}
-
-init();
 
 $('.sub-footer-order').on('click', function() {
     $('body').append('<div class="overlay-layer dark-layer"></div>');
@@ -33,52 +27,75 @@ $('.sub-footer-order').on('click', function() {
     }, {
         duration: 500,
         complete: function() {
-            $('.overlay-layer').on('click', cancel);
+            $('.overlay-layer').on('click', close);
         }
     });
-
-    $('.sub-order-btn-cancel').on('click', cancel);
-
-    function cancel() {
-        $('.sub-order-layer').animate({
-            bottom: '-161px'
-        }, {
-            duration: 500,
-            complete: function () {
-                $('.sub-order-layer').remove();
-                $('.overlay-layer').remove();
-                $('body').css('overflow', 'auto');
-            }
-        });
-    }
+    $('.sub-order-btn-cancel').on('click', close);
 
     $('.sub-order-option').on('click', function () {
+        $('.overlay-layer').off('click');
+        optionListInsert();
+
         $('.sub-order-text').append('<div class="overlay-layer dark-layer"></div>');
         $('.sub-order-text').css('overflow', 'hidden');
         $('.sub-order-option-list').css('display', 'block');
 
         $('.sub-order-option-list').animate({
             marginTop: '-100px',
-            height: '200px'
+            height: options.length * 42 + 'px'
         }, {
             duration: 500,
             complete: function () {
-
+                // 이벤트 걸때 - optionListClose() 함수로 주면 결과를 실행하기 때문에 자동으로 clsoe됨 - 중요!!
+                $('.overlay-layer').on('click', optionListClose);
             }
-        });
-
-        $('.sub-order-text .overlay-layer').on('click', function () {
-            $('.sub-order-option-list').animate({
-                marginTop: '0px',
-                height: '0px'
-            }, {
-                duration: 500,
-                complete: function () {
-                    $('.sub-order-option-list').css('display', 'none');
-                    $('.sub-order-text .overlay-layer').remove();
-                    $('.sub-order-text').css('overflow', 'auto');
-                }
-            });
         });
     });
 });
+
+function close() {
+    $('.sub-order-layer').animate({
+        bottom: '-161px'
+    }, {
+        duration: 500,
+        complete: function () {
+            $('.sub-order-layer').remove();
+            $('.overlay-layer').remove();
+            $('body').css('overflow', 'auto');
+        }
+    });
+}
+
+function optionListClose() {
+    $('.sub-order-option-list').animate({
+        marginTop: '0px',
+        height: '0px'
+    }, {
+        duration: 500,
+        complete: function () {
+            $('.sub-order-option-list').css('display', 'none');
+            $('.sub-order-text .overlay-layer').remove();
+            $('.sub-order-text').css('overflow', 'auto');
+
+            $('.overlay-layer').off('click');
+            $('.overlay-layer').on('click', close);
+        }
+    });
+}
+
+function optionListInsert() {
+    $('.sub-order-option-list').empty();
+    for (var i=0; i<options.length; i++) {
+        var html = '<li>' + options[i].option + '</li>';
+        $('.sub-order-option-list').append(html);
+    }
+
+    $('.sub-order-option-list > li').on('click', function () {
+        var index = $(this).index();
+        var value = $(this).text();
+
+        console.log(index);
+
+        optionListClose();
+    });
+}
